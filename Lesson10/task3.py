40695dab5ef3807a9188de94218941b34e6be857
Vlad <gvandreevich@icloud.com>
2015-11-17 12:19:15 +0300

    New changes

---
+++ Lesson10/task3.py
@@ -0,0 +1,40 @@
+class Point:
+    def __init__(self,line='0,0'):
+        self.x, self.y = [float(i) for i in line.split(',')]
+    def __str__(self):
+         return 'x = ' + str(self.x) + ' y = ' + str(self.y)    
+    def __add__(self, other):
+               return Point(self.x + other.x, self.y + other.y)
+    def __sub__(self, other):
+                return Point(self.x - other.x, self.y - other.y)
+    def __mul__(self, other):
+                return Point(self.x * other.x, self.y * other.y)
+    def __truediv__(self, other):
+                return Point(self.x / other.x, self.y / other.y)
+    def __floordiv__(self, other):
+                return Point(self.x // other.x, self.y // other.y)                               
+    def __mod__(self, other):
+                return Point(self.x % other.x, self.y % other.y)  
+    def __pow__(self, other):
+                return Point(self.x ** other.x, self.y ** other.y)  
+    def __abs__(self):
+                return (int(self.x)**2 + int(self.y)**2)**0.5
+         
+    
+print('!!!КООРДИНАТЫ ЦЕНТРА МАСС ДАННОГО МНОЖЕСТВА ТОЧЕК !!!')
+print(' кол-во точек') 
+N=int(input())
+max=0
+sumX=0
+sumY=0
+Xc,Yc=0,0
+for i in range(N):
+    A=Point(input())
+    sumX,sumY=A.x,A.y
+    Xc+=sumX/N
+    Yc+=sumY/N
+
+print('Координыты центра  масс {', Xc,',',Yc,'}')
+
+
+ 
--- Lesson9/task3.py
+++ Lesson9/task3.py
@@ -1,10 +1,34 @@
 
-f=open('input.txt')
-ourWords=[]
+words=open('TextForTask3.txt','r')
+stat={}
+for word in words.read().split():
+    word=word.replace(',','').replace('.','').replace(':','').replace('!','').replace('"','').replace('?','').replace('-','').replace(')','').replace('(','')
+    word=word.lower()
+    if word in stat:
+          stat[word]+=1
+    else:
+        stat[word]=1
+count=0
+for key in stat:
+   if stat[key] > count:
+      count=stat[key]
+   
+   print(key, stat[key])
+PopularWords=[]
+for key in stat:
+    if stat[key]==count:
+       PopularWords.append(stat[key])
+       PopularWords.append(key)
+ 
 
-for word in f.read().split():
-   ourWords.append(word)
-B={}
-for elem in ourWords:
-  B[(elem)]=ourWords.count(elem)
-print(B)
+
+PopularWords=PopularWords[::-1]
+print('!!! Наиболее встречаемые слова !!! :')
+
+for i in range(0,int(len(PopularWords)/2)+1,2):
+    print(PopularWords[i], end=' ')
+    print(PopularWords[i+1], 'раза')
+
+
+
+
